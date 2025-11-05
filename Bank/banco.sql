86054c050e7c56a5428dcbf3433dfe48ddd7c9c5
GABRIEL DE CAMARGO MIRANDA <2901392513032@fatecararas.com>
2025-11-04 21:39:11 -0300

    Adicionando coisas simples

--- Bank/banco.sql
+++ Bank/banco.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 29/10/2025 às 02:28
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+CREATE DATABASE IF NOT EXISTS banco;
+USE banco;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `banco`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tarefas`
---
 
 CREATE TABLE `tarefas` (
   `id` int(11) NOT NULL,
@@ -37,11 +15,7 @@ CREATE TABLE `tarefas` (
   `data_conclusao` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
--- Estrutura para tabela `usuario`
---
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
@@ -49,51 +23,25 @@ CREATE TABLE `usuario` (
   `senha` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices de tabela `tarefas`
---
 ALTER TABLE `tarefas`
   ADD PRIMARY KEY (`id`),
   ADD KEY `usuario_id` (`usuario_id`);
 
---
--- Índices de tabela `usuario`
---
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
 
---
--- AUTO_INCREMENT de tabela `tarefas`
---
 ALTER TABLE `tarefas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `usuario`
---
+
 ALTER TABLE `usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Restrições para tabelas despejadas
---
 
---
--- Restrições para tabelas `tarefas`
---
 ALTER TABLE `tarefas`
   ADD CONSTRAINT `tarefas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
