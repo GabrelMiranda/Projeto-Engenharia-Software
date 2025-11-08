fe85d56d159b889f65d4c1440a620cdc5d81b67f
Heitor <fernandesheitort2@gmail.com>
2025-11-08 17:43:00 -0300

    Corrigi pequeno detalhe dentro de cadastro de usuario

--- projeto-tarefas/backend/cadastro.php
+++ projeto-tarefas/backend/cadastro.php
@@ -6,7 +6,7 @@ $email = $_POST['email'];
 $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 
 try {
-    $stmt = $pdo->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)");
+    $stmt = $pdo->prepare("INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)");
     $stmt->execute([$nome, $email, $senha]);
     header("Location: ../Frontend/pages/index.html?sucesso=1");
 } catch (PDOException $e) {
